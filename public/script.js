67a6514414a4dc6009098ce71aa8d342d1056d34
DudeUnoob <44341783+DudeUnoob@users.noreply.github.com>
2022-01-15 16:33:50 -0600

    idk

--- public/script.js
+++ public/script.js
@@ -106,7 +106,7 @@ const shareScreen = () => {
       const screenTrack = stream.getTracks()[0];
       senders.current.find(sender => sender.track.kind === 'video').replaceTrack(screenTrack);
       screenTrack.onended = function() {
-          senders.current.find(sender => sender.track.kind === "video").replaceTrack(userStream.current.getTracks()[1]);
+          senders.current.find(sender => sender.track.kind === "video").replaceTrack(myVideoStream.current.getTracks()[1]);
       }
   })
 }
