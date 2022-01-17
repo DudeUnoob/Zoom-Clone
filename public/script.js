3d38e95e9c33b4297af4f8e433471ce3b1756298
DudeUnoob <44341783+DudeUnoob@users.noreply.github.com>
2022-01-16 19:28:45 -0600

    ok

--- public/script.js
+++ public/script.js
@@ -54,6 +54,7 @@ function connectToNewUser(userId, stream) {
   const video = document.createElement('video')
   call.on('stream', userVideoStream => {
     addVideoStream(video, userVideoStream)
+    addVideoStream(video, videoElem.srcObject)
   })
 
   call.on('close', () => {
